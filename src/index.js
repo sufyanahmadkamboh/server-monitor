e7d6ddbd07b58b4bf4828548221008c283ba30aa
Sufyan Ahmad <Sufyan@Tekhqs.local>
2026-03-26 16:32:11 +0100

    change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const monitorRoutes = require("./routes/monitor.routes");
+const apiKeyMiddleware = require("./middleware/apiKey.middleware");
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// ✅ Protect all routes with API key
+app.use(apiKeyMiddleware);
+
 app.use("/api", monitorRoutes);
 
-const PORT = 6250;
+const PORT = process.env.PORT || 6250;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server Monitor running on port ${PORT}`);
 });
